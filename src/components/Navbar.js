ba058a2f4392831a8cacf88291c0b812dfeaccda
Gabriela M. Fernández <galalou2010@gmail.com>
2022-12-13 14:32:43 +0100

    navbar and general styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,18 @@
-
+import '../styles/index.css'
+import logo from "../img/logo.svg"
 
 function Navbar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className='navbar'>
+      <div className="navbar__company">
+        <img src={logo} alt="Soundwave logo" className='navbar__logo'></img>
+        <p className="navbar__item">Soundwave</p>
+      </div>
+        <ul className='navbar__list'>
+          <li className='navbar__item navbar-options'>Discover</li>
+          <li className='navbar__item navbar-options'>Join</li>
+        </ul> 
+    </nav>
   );
 }
 
