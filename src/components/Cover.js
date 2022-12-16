ad3b7810cff4f60de9cb18f17feb5edef8181de1
Gabriela M. Fernández <galalou2010@gmail.com>
2022-12-16 10:46:09 +0100

    "added missing alt to image"

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -6,7 +6,7 @@ import "../img/bg/bg-laptop-1.png"
 function Cover() {
   return (
     <div className='cover'>
-      <img src={girl} className='cover__girl'></img>
+      <img src={girl} className='cover__girl' alt="cover girl"></img>
       <div classname='cover-box'>
       <h1 className='cover__title'>Feel The Music</h1>
       <h2 className='cover__slogan'>Stream over 20 thousand songs with one click</h2>
